ebfb172205187c327d3bbcd878b597ae04872033
Alien <lmllewellyn@gmail.com>
2024-07-14 20:17:57 -0400

    fixed prompt chain, imported fs in index.js, added logos into examples folder, added if statement for square shape

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const templateSVG = (response) => {
+    if (response.shape === 'circle') {
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        
+        <circle cx="150" cy="100" r="80" fill="${response.bgColor}" />
+        
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+        
+        </svg>`
+        } else if (response.shape === 'square') {
+            return `<svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+
+  <rect width="115" height="100" x="10" y="10" rx="20" ry="20" fill="${response.bgColor}" />
+
+  <text x="70" y="80" font-size="50" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+  
+</svg>`
+        
+}
+}
 
 inquirer
     .prompt([
         {
             type: "input",
-            message: "Which 3 letters do you want in your log?",
+            message: "Which 3 letters do you want in your logo?",
             name: "text",
         },
         {
@@ -13,9 +35,19 @@ inquirer
             name: "textColor",
         },
         {
+            type: "input",
+            message: "What color do you want the background to be?",
+            name: "bgColor",
+        },
+        {
             type: "list",
             message: "Pick a shape for your logo",
+            name: "shape",
             choices: ['circle', 'triangle', 'square'],
         }
     ])
-    .then(response => console.log(response));
+    .then((response) => {
+        const result = templateSVG(response);
+        fs.writeFileSync('logo.svg', result);
+    })
+
